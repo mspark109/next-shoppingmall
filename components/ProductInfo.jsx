1799140606818be45ed2c657abbceba3f62ba64f
mspark109 <minsup109@gmail.com>
2024-07-30 23:30:24 +0900

    useRouter 페이지 이동 추가

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import Styles from "./ProductInfo.module.css";
+import { useRouter } from "next/router";
 
 export default function ProductInfo({productDetail}) {
+  const router = useRouter();
+
+  const addCart = () => {
+    // 1. 장바구니에 아이템 추가 
+
+    //2.장바구니로 이동
+    router.push('/cart');
+  } 
+
   return (
     <div className={Styles.container}>
       <div>
@@ -10,7 +20,7 @@ export default function ProductInfo({productDetail}) {
       <div className={Styles.description}>
         <p>name: {productDetail.name}</p>
         <p>price: {productDetail.price}</p>
-        <button>장바구니에 담기</button>
+        <button onClick={addCart}>장바구니에 담기</button>
       </div>
     </div>
   )
